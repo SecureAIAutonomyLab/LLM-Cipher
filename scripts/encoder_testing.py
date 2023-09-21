67cb08a1a9706b6e3bd9b117f8b309e077b5037f
BrandonWherry <bwh1126@gmail.com>
2023-09-21 14:43:42 +0000

    adding some embeddings

--- scripts/encoder_testing.py
+++ scripts/encoder_testing.py
@@ -1,26 +1,5 @@
-from transformers import T5Config, T5EncoderModel, T5Tokenizer
+from transformers import T5EncoderModel, T5Tokenizer, AutoTokenizer
 
-config = T5Config()
+encoder = T5EncoderModel.from_pretrained('google/flan-t5-xl').to(device=0)
 
-encoder = T5EncoderModel(config)
-
-tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
-
-article_body = (
-    "In a surprising turn of events, scientists at the fictitious Pangea University "
-    "announced yesterday that they have discovered a new species of chameleon in the "
-    "heart of New York City's Central Park. The 'Urban Chameleon', as it's being "
-)
-
-tokenized_text = tokenizer(article_body, return_tensors='pt', padding='max_length', max_length=512)
-
-print(len(tokenized_text.input_ids[0]))
-
-embeddings = encoder.forward(input_ids=tokenized_text.input_ids, attention_mask=tokenized_text.attention_mask)
-
-print(embeddings.keys())
-
-print(embeddings['last_hidden_state'].shape)
-
-print(embeddings['last_hidden_state'][0][260].shape)
-
+print(encoder.config)
--- scripts/generate_embeddings_from_data.py
+++ scripts/generate_embeddings_from_data.py
@@ -1,46 +1,65 @@
 import json
 import os
 import glob
-from transformers import T5Config, T5EncoderModel, T5Tokenizer
+from transformers import T5EncoderModel, T5Tokenizer, AutoTokenizer
 
-config = T5Config()
-encoder = T5EncoderModel(config)
-tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
+encoder = T5EncoderModel.from_pretrained('google/flan-t5-xl').to(device=0)
+tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-xl")
 
 def encode_text(text):
     """
     Returns the last hidden state of the given text using the T5 encoder.
     """
-    tokenized_text = tokenizer(text, return_tensors='pt', padding='max_length', max_length=512)
+    if isinstance(text, list):
+        text = text[0]
+    assert isinstance(text, str), "Text is not a str!"   
+    tokenized_text = tokenizer(text, return_tensors='pt', max_length=512, truncation=True).to(device=0)
     embeddings = encoder.forward(input_ids=tokenized_text.input_ids, attention_mask=tokenized_text.attention_mask)
     return embeddings['last_hidden_state'][0][-1].tolist()
 
 source_directory = '../data/'
 destination_directory = '../embedding_data/' 
-allowed_prefixes = ['wikihow', 'wikipedia', 'peerread', 'reddit', 'arxiv']
+allowed_prefixes = ['wikihow_cohere', 'wikipedia', 'peerread', 'reddit', 'arxiv']
 
 os.makedirs(destination_directory, exist_ok=True)
 
 filepaths = sum([glob.glob(f"{source_directory}{prefix}*.jsonl") for prefix in allowed_prefixes], [])
 
 for filepath in filepaths:
+    print(f'Starting {filepath}')
     with open(filepath, 'r') as infile:
         filename = os.path.basename(filepath).split('.')[0]
         
-        new_filename = os.path.join(destination_directory, filename + '_embedding.jsonl')
+        new_filename = os.path.join(destination_directory, filename + '_emb.jsonl')
         
         with open(new_filename, 'w') as outfile:
-            for line in infile:
+            for line_number, line in enumerate(infile):
+                if len(line) <= 20:
+                    continue
                 row = json.loads(line)
-                if 'human_text' in row and 'machine_text' in row:
-                    human_embedding = encode_text(row['human_text'])
-                    machine_embedding = encode_text(row['machine_text'])
 
-                    embedding_data = {
-                        'human_text_embedding': human_embedding,
-                        'machine_text_embedding': machine_embedding
-                    }
+                human_embedding = None
+                machine_embedding = None
 
-                    outfile.write(json.dumps(embedding_data) + '\n')
+                key_pairs = [
+                    ('human_text', 'machine_text'),
+                    ('text', 'machine_text'),
+                    ('abstract', 'machine_abstract'),
+                    ('human_reviews', 'bloom_reviews')
+                ]
+
+                for human_key, machine_key in key_pairs:
+                    if human_key in row and machine_key in row:
+                        human_embedding = encode_text(row[human_key])
+                        machine_embedding = encode_text(row[machine_key])
+                        break
+                
+                assert human_embedding, "keys weren't found!"
+                embedding_data = {
+                    'human_text_embedding': human_embedding,
+                    'machine_text_embedding': machine_embedding
+                }
+
+                outfile.write(json.dumps(embedding_data) + '\n')
 
 print("Processing complete!")
